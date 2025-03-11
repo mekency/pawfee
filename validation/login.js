fcb24ffdca33b085c398b84c13304174e728a425
mouloud-2001 <2723205@collegelacite.ca>
2025-03-11 13:03:01 -0400

    correction des erreur de la validation

--- validation/login.js
+++ validation/login.js
@@ -5,7 +5,6 @@ export function validateLogin(formData) {
     const courriel = formData.get('courriel');
     const municipalite = formData.get('municipalite');
     const message = formData.get('message');
-<<<<<<< HEAD
   
     // Affichage des résultats dans la console
     console.log('Nom:', nom);
@@ -69,51 +68,4 @@ export function validateLogin(formData) {
     // Pour le moment, retourne un résultat par défaut
     return [null, {}];
   }
-=======
-
-    // Initialisation de l'état du formulaire avec erreurs nulles
-    let newState = {
-        nom: { valeur: '', erreur: null },
-        prenom: { valeur: '', erreur: null },
-        telephone: { valeur: '', erreur: null },
-        courriel: { valeur: '', erreur: null },
-        municipalite: { valeur: '', erreur: null },
-        message: { valeur: '', erreur: null }
-    };
-
-    let erreur = false;
-    // Validation des champs
-    if (!prenom) {
-        erreur = true;
-        newState.prenom.erreur = 'Veuillez entrer votre prénom';
-    }
-    if (!nom) {
-        erreur = true;
-        newState.nom.erreur = 'Veuillez entrer votre nom';
-    }
-    if (!telephone) {
-        erreur = true;
-        newState.telephone.erreur = 'Veuillez entrer un numéro de téléphone valide.';
-    } else if (!telephone.match(/^\d{10}$/)) {
-        erreur = true;
-        newState.telephone.erreur = 'Veuillez entrer un numéro de téléphone de 10 chiffres.';
-    }
-    if (!courriel) {
-        erreur = true;
-        newState.courriel.erreur = 'Veuillez entrer une adresse courriel.';
-    } else if (!courriel.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-        erreur = true;
-        newState.courriel.erreur = 'Veuillez entrer une adresse courriel valide.';
-    }
-    if (!municipalite) {
-        erreur = true;
-        newState.municipalite.erreur = 'Veuillez entrer votre municipalité';
-    }
-    if (!message) {
-        erreur = true;
-        newState.message.erreur = 'Veuillez entrer un message';
-    }
-
-    return [erreur, newState];
-} 
->>>>>>> 0292de453d64ae7b318c5c32212a852bcffe8517
+  
